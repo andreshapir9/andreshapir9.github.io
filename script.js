e28db37208491417c07a71a524b0aaa347ed3dcd
andre shapiro <shapiroandre@gmail.com>
2019-07-18 23:24:22 -0700

    test the click me

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ function addnum(){
 	window.alert("the answer for " + x + " + " + y + " is: " + (x+y));
 }
 
+function subnum(){
+	        var x = parseFloat(document.getElementById('numberin1').value);
+	        var y = parseFloat(document.getElementById('numberin2').value);
+	        window.alert("the answer for " + x + " - " + y + " is: " + (x-y));
+}
+
+function multnum(){
+	                var x = parseFloat(document.getElementById('numberin1').value);
+	                var y = parseFloat(document.getElementById('numberin2').value);
+	                window.alert("the answer for " + x + " * " + y + " is: " + (x*y));
+}
+function divnum(){
+	                var x = parseFloat(document.getElementById('numberin1').value);
+	                var y = parseFloat(document.getElementById('numberin2').value);
+	                window.alert("the answer for " + x + " / " + y + " is: " + (x/y));
+}
+function pownum(){
+	                var x = parseFloat(document.getElementById('numberin1').value);
+	                var y = parseFloat(document.getElementById('numberin2').value);
+	                window.alert("the answer for " + x + " - " + y + " is: " + (Math.pow(x, y)));
+}
