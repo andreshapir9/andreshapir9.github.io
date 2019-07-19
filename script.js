1c468c18ecdfce6ae4e8e1ea362c202627b8cd58
andre shapiro <shapiroandre@gmail.com>
2019-07-19 09:38:18 -0700

    making a dropdown work

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function Hello() {
 function sel_op(){
 	var a = document.getElementById("thedropdown");
 	alert(a.options[a.selectedIndex].value);
-	if(a = "+"){
+	if(a == "+"){
 		addnum();
 		window.alert("lololololl" + "the variable is " + a);
 	}else if(a == "-"){
