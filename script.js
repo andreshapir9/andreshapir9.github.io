62d8f74ab89aa56abf621bedb232dc5a507c559e
andre shapiro <shapiroandre@gmail.com>
2019-07-19 00:05:17 -0700

    test the click me

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function Hello() {
 }
 
 function sel_op(){
-	var a = document.getElementById("thedropdown");
+	var a = (document.getElementById("thedropdown").value);
 	alert(a.options[a.selectedIndex].value);
 	if(a = '+'){
 		addnum();
