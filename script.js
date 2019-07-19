5f65acbcae6dd4e53770d0684a39bcf48dba2215
andre shapiro <shapiroandre@gmail.com>
2019-07-18 22:54:59 -0700

    test the click me

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function Hello() {
 	window.alert("Hello, World");
 }
+function addnum(){
+	var x = parsefloat(document.getElementById('numberin1').value);
+	var x = parsefloat(document.getElementById('numberin2').value);
+	window.alert("the answer for " + x " +" + y + "is" +(x+y));
+}
 
